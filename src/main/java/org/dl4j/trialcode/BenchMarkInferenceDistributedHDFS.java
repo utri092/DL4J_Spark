bf054674cf93a943ede656625cc06b4e7c22e07a
Utsav <utri092@aucklanduni.ac.nz>
2020-10-08 20:25:39 +1300

    Add trial Code BenchMarkInferenceDistributed who=ich reads from local directory.

--- src/main/java/org/dl4j/trialcode/BenchMarkInferenceDistributedHDFS.java
+++ src/main/java/org/dl4j/trialcode/BenchMarkInferenceDistributedHDFS.java
@@ -1,5 +1,7 @@
 package org.dl4j.trialcode;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -8,9 +10,6 @@ import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.writable.Writable;
 import org.datavec.spark.transform.misc.StringToWritablesFunction;
-import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.spark.api.TrainingMaster;
 import org.deeplearning4j.spark.datavec.DataVecDataSetFunction;
@@ -21,67 +20,41 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import scala.Tuple2;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 
-public class BenchMarkInferenceDistributed {
-
-    private static Object PortableDataStream;
+public class BenchMarkInferenceDistributedHDFS {
 
     public static JavaSparkContext startSparkSession(){
         SparkConf conf = new SparkConf();
-        conf.setAppName("DL4JTrain");
-//        conf.setMaster("local[*]");
+        conf.setAppName("DL4JInferenceDistributed");
+        //conf.setMaster("local[*]");
         conf.setMaster("spark://192.168.137.224:7077");
 
         return new JavaSparkContext(conf);
     }
 
-    public static SparkDl4jMultiLayer createModel(JavaSparkContext sc, String modelPath){
-        MultiLayerNetwork model = null;
-        try {
-            model = KerasModelImport.importKerasSequentialModelAndWeights(modelPath, true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InvalidKerasConfigurationException e) {
-            e.printStackTrace();
-        } catch (UnsupportedKerasConfigurationException e) {
-            e.printStackTrace();
-        }
+    public static SparkDl4jMultiLayer createModelFromBin(String modelPath, JavaSparkContext sc) throws IOException, URISyntaxException {
+        //@detail Takes in HDFS string path and tries to get model.bin
 
-        TrainingMaster tm = new ParameterAveragingTrainingMaster.Builder(1).build();
-
-//        System.out.println(model.getLayers());
-//        System.out.println(model.getLayerWiseConfigurations());
-
-        return new SparkDl4jMultiLayer(sc, model, tm);
-
-    }
-
-    public static SparkDl4jMultiLayer createModelFromBin(JavaSparkContext sc, String modelPath){
         MultiLayerNetwork model = null;
         MultiLayerNetwork net = null;
-        
-        try {
 
-            File file = new File(modelPath);
+        FileSystem fileSystem = FileSystem.get(new URI ("hdfs://afog-master:9000"), sc.hadoopConfiguration());
 
-            InputStream targetStream = new FileInputStream(file);
-            
-            try(BufferedInputStream is = new BufferedInputStream(targetStream)){
-                net = ModelSerializer.restoreMultiLayerNetwork(is);
-            }
-            
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        try(BufferedInputStream is = new BufferedInputStream(fileSystem.open(new Path(modelPath)))){
+            net = ModelSerializer.restoreMultiLayerNetwork(is);
         }
-
 
         TrainingMaster tm = new ParameterAveragingTrainingMaster.Builder(1).build();
 
         SparkDl4jMultiLayer sparkNet = new SparkDl4jMultiLayer(sc, net, tm);
+
+
 
         return sparkNet;
 
@@ -97,14 +70,14 @@ public class BenchMarkInferenceDistributed {
         int lastColumnLabel = 1;    // No of labels after the last feature column
         JavaRDD<DataSet> testData = rddWritables.map(new DataVecDataSetFunction(firstColumnLabel, lastColumnLabel, true, null, null));
 
-        for (DataSet dataSet : testData.collect()) {
+       /* for (DataSet dataSet : testData.collect()) {
             System.out.println(dataSet.getFeatures());
-        }
+        }*/
 
         return testData;
     }
 
-    public static JavaPairRDD<String, INDArray> makePredictions(JavaPairRDD<String, INDArray> testPairs, SparkDl4jMultiLayer sparkNet){
+    public static JavaPairRDD<String, INDArray> makePredictions( JavaPairRDD<String, INDArray> testPairs, SparkDl4jMultiLayer sparkNet){
         // @detail Tuple2: Scala class expects two arguments. Tuple3 and Tuple4 are alternatives
         // @arg-1: Name of label/s
         // @arg-2: INDArray of features from JavaRDD<Dataset>
@@ -116,17 +89,17 @@ public class BenchMarkInferenceDistributed {
 
     public static void main(String[] args) throws Exception {
 
+
         int iterations = 1000;
 
         JavaSparkContext sc = startSparkSession();
 
-        String localModelPath = "./src/main/resources/benchmarks/model.bin";
 
-//        SparkDl4jMultiLayer sparkNet = createModel(sc, localModelPath);
+        String localModelPath = "hdfs://afog-master:9000/part4-projects/resources/benchmarks/model.bin";
 
-        SparkDl4jMultiLayer sparkNet = createModelFromBin(sc, localModelPath);
+        SparkDl4jMultiLayer sparkNet = createModelFromBin(localModelPath, sc);
 
-        String datafilePath = "./src/main/resources/benchmarks/dataset-1_converted.csv";
+        String datafilePath = "hdfs://afog-master:9000/part4-projects/resources/benchmarks/dataset-1_converted.csv";
 
         JavaRDD<DataSet> testData = extractTestDataset(datafilePath, sc);
 
@@ -137,7 +110,7 @@ public class BenchMarkInferenceDistributed {
         JavaPairRDD<String, INDArray> testPairs = testData.mapToPair(f-> new Tuple2("carparkOccupancy", f.getFeatures()));
 
         for(int i = 0 ; i < iterations; i++){
-           JavaPairRDD<String, INDArray> predictions = makePredictions(testPairs, sparkNet);
+            JavaPairRDD<String, INDArray> predictions = makePredictions(testPairs, sparkNet);
         }
 
         long endTime = System.nanoTime();
@@ -145,6 +118,8 @@ public class BenchMarkInferenceDistributed {
         long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds
         System.out.println(duration/1000000000);
         System.out.println("DONE Inferencing");
+
+        System.out.println(sparkNet.getNetwork().getLayerWiseConfigurations());
 
     }
 
